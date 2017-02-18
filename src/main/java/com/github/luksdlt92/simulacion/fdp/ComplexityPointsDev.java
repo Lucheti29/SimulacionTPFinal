5e2ba27abe3d109f65761e2aa781656ea1acbece
Lucas <luksdlt92@gmail.com>
2017-02-18 15:43:53 -0300

    - Mocking FDPs - Refactor

---
+++ src/main/java/com/github/luksdlt92/simulacion/fdp/ComplexityPointsDev.java
@@ -0,0 +1,12 @@
+package com.github.luksdlt92.simulacion.fdp;
+
+public class ComplexityPointsDev {
+
+    public static double getEstimatedPointsPerSprint(int technology) {
+        return 0;
+    }
+
+    public static double getCompletedPointsPerHour(int technology, int seniority) {
+        return 0;
+    }
+}

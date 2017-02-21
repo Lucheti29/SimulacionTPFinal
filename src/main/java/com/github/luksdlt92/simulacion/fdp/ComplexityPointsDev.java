4a7282fd8b703a3ea653bc1c49c18d34a0c82409
Federico <fede21perez@hotmail.com>
2017-02-21 17:15:35 -0300

    Agrego las fdp de Android al programa

--- src/main/java/com/github/luksdlt92/simulacion/fdp/ComplexityPointsDev.java
+++ src/main/java/com/github/luksdlt92/simulacion/fdp/ComplexityPointsDev.java
@@ -1,12 +1,46 @@
 package com.github.luksdlt92.simulacion.fdp;
 
+import com.github.luksdlt92.simulacion.constant.*;
+import static java.lang.Math.*;
+
 public class ComplexityPointsDev {
 
     public static double getEstimatedPointsPerSprint(int technology, int seniority) {
+    	
+    	double r = Math.random();
+    	double u;
+    	double o;
+    	
+    	if( technology == Technology.ANDROID){
+    		
+    		if( seniority == Seniority.JUNIOR ){
+    			o = 0.08766;
+    			u = 0.4473;
+    			return r * tan(0.5 * PI * (2*o - 1) ) + u;
+    		}
+    		
+    		if( seniority == Seniority.SENIOR ){
+    			o = 0.32013;
+    			u = 0.56187;
+    			return u - r * log(-Math.log(o));
+    		}
+    		
+    		if( seniority == Seniority.SEMISENIOR ){
+    			double k = 0.21769;
+    			o = 0.2153;
+    			u = 0.40632;
+    			return ( pow(-log(k), -r) * (o * pow(-log(k),r) - u*r * (pow(-log(k),r) - o))  )/ r;
+    		}
+    		
+    	}
+    	
         return 0;
     }
 
     public static double getCompletedPointsPerHour(int technology, int seniority) {
+    	
+    	
+    	
         return 0;
     }
 }

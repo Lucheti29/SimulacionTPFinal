79761c9624431b71a54898eb38b5042494a2a3d4
Federico <fede21perez@hotmail.com>
2017-02-21 23:39:15 -0300

    Agrego fdp QA.

--- src/main/java/com/github/luksdlt92/simulacion/fdp/ComplexityPointsQA.java
+++ src/main/java/com/github/luksdlt92/simulacion/fdp/ComplexityPointsQA.java
@@ -1,8 +1,17 @@
 package com.github.luksdlt92.simulacion.fdp;
 
+import static java.lang.Math.*;
+
 public class ComplexityPointsQA {
 
     public static double getPointsTestedPerHour() {
-        return 0;
+    	
+    	double k = 0.53008;
+    	double a = 2.5674;
+    	double b = 15.176;
+    	double y = 0.10339;
+    	double r = random();
+    	
+        return pow( (pow(a,-1/k)), -1/r) * (y * pow( (pow(a, -1/k) - 1), 1/r) + b);
     }
 }

3fc4e0c4c09703508ea99149795dc9fe6fb29fff
Lucas <luksdlt92@gmail.com>
2017-02-23 21:19:12 -0300

    Fix de las fdp de puntos estimados

--- src/main/java/com/github/luksdlt92/simulacion/fdp/ComplexityPointsDev.java
+++ src/main/java/com/github/luksdlt92/simulacion/fdp/ComplexityPointsDev.java
@@ -15,12 +15,12 @@ public class ComplexityPointsDev {
     	double rParcial = 0;
 		
     	if( technology == Technology.ANDROID )
-    		rParcial = 100.8/pow( 1/pow(r,118.483) - 1 , 0.002660069693825978 ) * 0.62;
+    		rParcial = 100.8/pow( 1/pow(r,118.483) - 1 , 0.002660069693825978 ) * 0.01 * 0.62;
     	if( technology == Technology.IOS )
-    		rParcial = 140.7 * pow( 1/pow(1 - r, 0.0200582) - 1, 0.1487187876444431 ) * 0.78;
+    		rParcial = 140.7 * pow( 1/pow(1 - r, 0.0200582) - 1, 0.1487187876444431 ) * 0.01 * 0.78;
     	if( technology == Technology.WEB )
-    		rParcial = -107.522 * ( 0.0369413 - pow(1 - pow(1-r,0.380662),0.3364284753061499) ) * 0.8;
-	    
+    		rParcial = -107.522 * ( 0.0369413 - pow(1 - pow(1-r,0.380662),0.3364284753061499) ) * 0.01 * 0.8;
+
         return rParcial * hoursWork;
     }
     
